a06ceada89047e3897bef76f4f870dd3a63d4289
Rachit Garg <rachitgarg@Rachits-MacBook-Air.local>
2026-03-10 17:32:43 +0530

    removing cyclic dependency

--- src/main/java/com/vaultiq/vaultiq/config/SecurityConfig.java
+++ src/main/java/com/vaultiq/vaultiq/config/SecurityConfig.java
@@ -48,11 +48,6 @@ public class SecurityConfig {
     }
 
     @Bean
-    public PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
-    }
-
-    @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
         config.setAllowedOriginPatterns(List.of("*"));
